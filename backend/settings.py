8b9ed8194b1873b63e9aa89c2a694d2fe281c6f8
moncefWitcher <moncef.tayoub2@aiesec.net>
2022-10-25 17:51:30 +0100

    updating settings

--- backend/settings.py
+++ backend/settings.py
@@ -137,9 +137,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
