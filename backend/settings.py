012d7d18739e318fb5b334be39d1cfabc6b04cd2
moncefWitcher <moncef.tayoub2@aiesec.net>
2022-10-31 02:08:53 +0100

    git add all

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=rk)%$-*2ee@=ht!9(pfuh71j_$e@)yv1fnb!+v(j@+1hc=7-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
     
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [
