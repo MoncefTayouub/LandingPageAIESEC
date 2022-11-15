c7c852f31aa30e8fee67181b39312a7e70adbc5b
moncefWitcher <moncef.tayoub2@aiesec.net>
2022-11-15 15:24:39 +0100

    maby thats right

--- app/migrations/0006_auto_20221115_1515.py
+++ app/migrations/0006_auto_20221115_1515.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='mcteam',
             name='mcParent',
-            field=models.ForeignKey(default=datetime.date.today, on_delete=django.db.models.deletion.CASCADE, to='app.mc'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='app.mc'),
             preserve_default=False,
         ),
     ]
